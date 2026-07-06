9d7928770cf535503ee8ddc469dbc1e43c5d4752
Henning Holm <git@henningholm.de>
2026-07-06 19:56:06 +0200

    Update server type in create/delete server example
    The old server type has been deprecated.

--- examples/create_delete_servers.rs
+++ examples/create_delete_servers.rs
@@ -5,7 +5,7 @@ use rand::prelude::*;
 use std::{env, thread, time};
 
 // server type and location to be used for creating the servers
-const SERVER_TYPE: &str = "cx22";
+const SERVER_TYPE: &str = "cx23";
 const LOCATION: &str = "fsn1";
 const IMAGE: &str = "ubuntu-22.04";
 
